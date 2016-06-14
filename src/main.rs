634074ba091f2174ba4ccdf8a42e1343315b26ae
hyq <huangyangqi@gmail.com>
2016-06-14 19:35:05 +0800

    send

--- src/main.rs
+++ src/main.rs
@@ -23,20 +23,31 @@ struct WINDIVERT_ADDRESS {
 extern "C" {
     fn WinDivertOpen(f: *const c_char, l: WINDIVERT_LAYER, p: i16, flag: i64) -> HANDLE;
     fn WinDivertRecv(h: HANDLE, p: *mut u8, l: u32, a: *mut WINDIVERT_ADDRESS, rl: &u32) -> bool;
+    fn WinDivertSend(h: HANDLE,
+                     p: *const u8,
+                     l: u32,
+                     a: *const WINDIVERT_ADDRESS,
+                     sl: &u32)
+                     -> bool;
+    fn WinDivertClose(h: HANDLE) -> bool;
 }
 
-fn windivert_open(f: &CStr) -> Win32Result<HANDLE> {
+fn windivert_open(f: &CStr) -> Win32Result<WinDivert> {
     unsafe {
         let h = WinDivertOpen(f.as_ptr(), WINDIVERT_LAYER::WINDIVERT_LAYER_NETWORK, 0, 0);
         match h as i32 {
             -1 => Err(Win32Error::new()),
-            _ => Ok(h),
+            _ => Ok(WinDivert { handle: h }),
         }
     }
 }
 
-fn windivert_recv(h: HANDLE) -> Result<(Vec<u8>, WINDIVERT_ADDRESS, u32), i32> {
+fn windivert_recv(h: HANDLE) -> Result<(Vec<u8>, WINDIVERT_ADDRESS), i32> {
     let mut buf = Vec::with_capacity(5000);
+    {
+        let l = buf.capacity();
+        buf.resize(l, 0);
+    }
     let mut addr = WINDIVERT_ADDRESS {
         Direction: 0,
         IfIdx: 0,
@@ -47,16 +58,48 @@ fn windivert_recv(h: HANDLE) -> Result<(Vec<u8>, WINDIVERT_ADDRESS, u32), i32> {
 
         let ok = WinDivertRecv(h,
                                buf.as_mut_ptr(),
-                               buf.capacity() as u32,
+                               buf.len() as u32,
                                &mut addr as *mut WINDIVERT_ADDRESS,
                                &rl);
     }
-    Ok((buf, addr, rl))
+    buf.resize(rl as usize, 0);
+    Ok((buf, addr))
+}
+
+fn windivert_send(h: HANDLE, buf: &Vec<u8>, addr: &WINDIVERT_ADDRESS) -> Result<(u32), i32> {
+    let mut wl: u32 = 0;
+    unsafe {
+
+        let ok = WinDivertSend(h,
+                               buf.as_ptr(),
+                               buf.len() as u32,
+                               addr as *const WINDIVERT_ADDRESS,
+                               &wl);
+    }
+    Ok((wl))
+}
+
+
+struct WinDivert {
+    handle: HANDLE,
+}
+
+impl WinDivert {
+    fn recv(&self) -> Result<(Vec<u8>, WINDIVERT_ADDRESS), i32> {
+        windivert_recv(self.handle)
+    }
+    fn send(&self, buf: &Vec<u8>, addr: &WINDIVERT_ADDRESS) -> Result<(u32), i32> {
+        windivert_send(self.handle, buf, addr)
+    }
 }
 
 fn main() {
     println!("Hello, world!");
     let h = windivert_open(&CString::new("tcp").unwrap()).unwrap();
-    let (buf, addr, rl) = windivert_recv(h).unwrap();
-    println!("{}", rl);
+    loop {
+        let (mut buf, mut addr) = h.recv().unwrap();
+        println!("{}", buf.len());
+        let _ = h.send(&buf, &addr);
+    }
+
 }
